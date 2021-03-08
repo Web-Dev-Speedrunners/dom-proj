c51e70246b12b478bf8f2b9f0d8011ef23a27ab0
muskankapoor <muskankapoor016@gmail.com>
2021-03-08 17:32:15 -0500

    implement rremove row

--- js/removeCols.js
+++
@@ -1,8 +0,0 @@
-const removeCols = function() {
-   let removeCol = document.getElementsByClassName('cell');
-   for(let i = 0; i < removeCol.length - 1; i++){
-        removeCol[i].remove();  
-    }
-}
-
-
