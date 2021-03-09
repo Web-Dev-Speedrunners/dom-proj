a2e4c66309ccde86eb0c8e5f917e8afdbbb0cc83
Xuejin Gao <xuejingao@gmail.com>
2021-03-08 21:56:48 -0500

    Added some error messages

--- js/colorAllCells.js
+++ js/colorAllCells.js
@@ -1,6 +1,12 @@
 const colorAllCells = function () {
     const colorWell = document.getElementById('colorWell');
     let cells = document.getElementsByClassName('cell');
+    
+    if(cells.length == 0){
+        console.log('Unable to find cells');
+        return;
+    }
+    
     for(let i = 0; i < cells.length; i++){
         cells[i].style.backgroundColor = colorWell.value;
     }
